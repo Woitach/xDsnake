fb6f6dc928df16c6082605d3e170a792c7b287ec
Woitach <Woitach@users.noreply.github.com>
2020-09-10 13:20:56 +0200

    Posty

--- src/App.js
+++ src/App.js
@@ -3,24 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function SendPost(){
+    console.log("sendt");
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div className="head">
+
+      </div>
+      <header className="LRlist">
+          <div>
+            <img src="Logo.jpg" className="logo"></img>
+          </div>
       </header>
+      <main className="Leftsite">
+        <input if="post" type="text" className="tekst"></input>
+        <input type="button" onclick="SendPost()"></input>
+      </main>
+      <aside className="Rightsite">
+        xx
+      </aside>
     </div>
   );
+  
 }
 
 export default App;
